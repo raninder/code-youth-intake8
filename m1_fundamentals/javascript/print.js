a43e6c50d33762fc6eaeceff28cc0fa25f071f2f
Raninder Dhillon <raninderdhillon@gmail.com>
2022-11-24 11:05:25 -0500

    data types

---
+++ m1_fundamentals/javascript/print.js
@@ -0,0 +1,27 @@
+console.log("Welcome to Code Youth");
+
+let number;
+number = 5;
+
+let a = 10;
+console.log("number is",a,number);
+let b = 20;
+let c = a + b;
+console.log("sum of",a,"and",b,"is:",c);
+const pi=3.142;
+console.log("pi:",pi);
+
+
+//string
+let name1 = "John";
+console.log(typeof pi);
+let name2 = `Brenda`;
+console.log(name2);
+let check = true;
+let check1= (a==10)
+//boolean
+console.log(typeof check1);
+//object
+let student = {"name":"john"};
+console.log("student:",student);
+console.log(typeof student);
