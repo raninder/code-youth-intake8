83315277a34004b85e5c6cbde1a9856698954b98
Raninder Dhillon <raninderdhillon@gmail.com>
2022-11-29 09:24:09 -0500

    Create sum-numbers.js

---
+++ m1_fundamentals/javascript/sum-numbers.js
@@ -0,0 +1,9 @@
+let sum = 0;
+for(let i=1;i<=5;i++){
+	let num = parseInt(prompt("enter a number"));
+	sum+=num;  //sum = sum+num;
+}
+console.log("sum is:", sum);
+console.log("average is:",sum/5);
+
+// float ---3.5,6.7
