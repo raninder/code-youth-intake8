7df5f8858790fe1c1b0680b77adb4ab19da7ec28
Raninder Dhillon <raninderdhillon@gmail.com>
2022-11-29 11:28:17 -0500

    break and continue in sum of natural numbers

--- m1_fundamentals/javascript/sum-numbers.js
+++ m1_fundamentals/javascript/sum-numbers.js
@@ -1,6 +1,13 @@
 let sum = 0;
 for(let i=1;i<=5;i++){
 	let num = parseInt(prompt("enter a number"));
+	//come out of loop when we enter a negative number
+	// if(num<0){
+	// 	break;
+	// }
+	if(num<0){
+		continue;
+	}
 	sum+=num;  //sum = sum+num;
 }
 console.log("sum is:", sum);
