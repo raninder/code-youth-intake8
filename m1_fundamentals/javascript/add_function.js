978e0ab33360f7e23cd1bb520ea75d1a3fabe57f
Raninder Dhillon <raninderdhillon@gmail.com>
2022-11-30 17:56:34 -0500

    Create add_function.js

---
+++ m1_fundamentals/javascript/add_function.js
@@ -0,0 +1,21 @@
+function add(num1,num2){  // num1=3, num2=4
+	let sum  = num1+num2;
+	// console.log("sum is:",sum)
+	return sum;
+}
+
+console.log(add(3,4));
+
+// add(14,66);
+// add(20,44);
+
+function cube(num){
+	let res = num*num*num;
+	return res;
+	// console.log("result");
+}
+
+console.log(cube(22));  //console.log(res)
+// let result = cube(22);
+// console.log(result);
+// ex: cube(5) ->  125
