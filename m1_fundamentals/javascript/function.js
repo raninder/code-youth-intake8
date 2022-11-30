d2f57ccb8feff4bff09c9c915f5740f58434f970
Raninder Dhillon <raninderdhillon@gmail.com>
2022-11-30 17:57:00 -0500

    functions basics

---
+++ m1_fundamentals/javascript/function.js
@@ -0,0 +1,33 @@
+// function declaration
+// function name_of_function(param1,param2,...){
+// 	//code
+// }
+// function call/invoke
+// name_of_function(arg1, arg2....)
+
+
+// function display(){
+// 	console.log("Bob");
+// }
+
+
+// display();
+// display();
+// display();
+// display();
+
+
+function display(firstname){
+	console.log(firstname);
+}
+
+display("Alice");
+display("bob");
+display("carol");
+
+function displayName(first,last){
+	console.log("My name is",first,last);
+}
+
+
+displayName("Alice", "Dhillon")   
