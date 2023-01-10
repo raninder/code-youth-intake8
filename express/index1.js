711bf71953f10f4b3647db57b2fe0da8ce2c34f9
Raninder Dhillon <raninderdhillon@gmail.com>
2023-01-10 10:38:52 -0500

    accessing public ine express

--- express/index1.js
+++ express/index1.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 8080;
 app.set("view engine", "ejs")
+app.use(express.static("public"))
 
 app.get('/', (req,res)=>{
 	res.send("Home Page")
