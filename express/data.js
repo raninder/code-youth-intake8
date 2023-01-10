ef185a16c6668381270d237b02483f5071a6ba5a
Raninder Dhillon <raninderdhillon@gmail.com>
2023-01-10 10:38:16 -0500

    json data

---
+++ express/data.js
@@ -0,0 +1,17 @@
+const products = [
+	{ id:1,
+	name: "iphone",
+	price: 500,
+	url:"https://m.media-amazon.com/images/I/71THEph8UiL._AC_UL640_FMwebp_QL65_.jpg"
+	},
+	{ id:2,
+		name: "ipad",
+		price: 600
+	},
+	{ id:3,
+		name: "watch",
+		price: 300
+	}
+]
+
+module.exports = products
