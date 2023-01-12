1a207f0e56cb2dd403062ed74c6f48306a70c823
Raninder Dhillon <raninderdhillon@gmail.com>
2023-01-12 10:41:43 -0500

    Update db.js

--- express/db.js
+++ express/db.js
@@ -12,8 +12,46 @@ const getUsers = (req,res) => {
 		if(err){
 			throw error;
 		}
-		return res.json(results.rows);
+		res.render("test3",{data:results.rows})
+		// return res.json(results.rows);
 	})
 }
 
-module.exports = getUsers;
+const getUserById = (req,res) => {
+	const id = parseInt(req.params.id);
+	
+	pool.query('SELECT * FROM users where id=$1',[id],(err,results)=>{
+		if(err){
+			throw error;
+		}
+			return res.json(results.rows);
+	})
+}
+
+const createUser = (req,res)=> {
+	const name = req.body.uname;
+	const email = req.body.email;
+
+	pool.query('INSERT INTO users(name,email) VALUES($1,$2)',[name,email],(err,results)=>{
+		if(err){
+			throw error;
+		}
+		res.send(`new user added with name: ${name} and email: ${email}`)
+		
+	})
+}
+
+// const deleteUser = (req,res) => {
+// const id= parseInt(req.params.id);
+// pool.query('DELETE FROM user where id=$1',[id],(err,results)=>{
+// 	if(err){
+// 		throw error;
+// 	}
+// 	res.send(`user deleted with id: ${id}`)
+	
+// })
+// }
+
+
+
+module.exports = {getUsers,getUserById,createUser};
