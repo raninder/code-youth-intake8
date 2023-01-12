b2baca48faca41c6e7aca5561c59c6d2fe3b3c58
Raninder Dhillon <raninderdhillon@gmail.com>
2023-01-12 10:41:48 -0500

    Update index5.js

--- express/index5.js
+++ express/index5.js
@@ -1,20 +1,30 @@
 const express = require('express')
 const app = express()
-const getUsers = require('./db.js')
+// const {getUsers,getUserById,createUser} = require('./db.js')
+const db = require('./db.js')
+
 
 app.set("view engine","ejs")
 
 app.get('/',(req,res)=>
 res.render('form')
 )
-app.get('/users',getUsers);
+//get all users from database
+app.get('/users',db.getUsers);
+
+//get specified user from database
+app.get('/users/:id',db.getUserById);
 
 app.use(express.urlencoded());
-app.post('/save',(req,res)=>{
+// app.post('/save',(req,res)=>{
 // res.send("form submitted")
-const name = req.body.uname;
-const email = req.body.email;
-res.send(`Name: ${name} Email: ${email}`)
-})
+// const name = req.body.uname;
+// const email = req.body.email;
+// res.send(`Name: ${name} Email: ${email}`)
+// })
+
+app.post('/save',db.createUser);
+
+// app.delete('/users/delete/:id',deleteUser)
 
 app.listen(8000, ()=>console.log("server running"))
